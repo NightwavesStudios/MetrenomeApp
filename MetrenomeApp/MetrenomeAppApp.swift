34e3a82d62085247064039400690aa75b44e1783
Chris Henry <cwhenry101@gmail.com>
2025-08-21 07:38:34 -0400

    Initial Commit

---
+++ MetrenomeApp/MetrenomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetrenomeAppApp.swift
+//  MetrenomeApp
+//
+//  Created by Chris Henry on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MetrenomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetrenomeAppTests/MetrenomeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetrenomeAppTests.swift
+//  MetrenomeAppTests
+//
+//  Created by Chris Henry on 8/21/25.
+//
+
+import Testing
+@testable import MetrenomeApp
+
+struct MetrenomeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
